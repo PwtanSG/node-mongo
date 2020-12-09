489a2a62d41be456d32c1fd6b344b1d554097d86
pwtan <pwtan.edu@gmail.com>
2020-12-09 14:54:15 +0800

    Wk2-node-mongo-ex2

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const MongonClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const dboper = require('./operations')
 
 const url = 'mongodb://localhost:27017';
 const dbname = 'conFusion';
 
+/*
 MongonClient.connect(url,(err, client) => {
     assert.equal(err,null);
     console.log('Connected correctly to server');
@@ -33,6 +35,36 @@ MongonClient.connect(url,(err, client) => {
             });
         });
     }); 
+});
+*/
+MongonClient.connect(url,(err, client) => {
+    assert.equal(err,null);
+    console.log('Connected correctly to server');
 
+    const db = client.db(dbname);
+    const collection = db.collection("dishes");
+    dboper.insertDocument(db, { name: "Vadonut", description: "Test"},
+    "dishes", (result) => {
+        console.log("Insert Document:\n", result.ops);
 
+        dboper.findDocuments(db, "dishes", (docs) => {
+            console.log("Found Documents:\n", docs);
+
+            dboper.updateDocument(db, { name: "Vadonut" },
+                { name: "Updated Test" }, "dishes",
+                (result) => {
+                    console.log("Updated Document:\n", result.result);
+
+                    dboper.findDocuments(db, "dishes", (docs) => {
+                        console.log("Found Updated Documents:\n", docs);
+                        
+                        db.dropCollection("dishes", (result) => {
+                            console.log("Dropped Collection: ", result);
+
+                            client.close();
+                        });
+                    });
+                });
+        });
+    });
 });
